789fca0146989f6b531e0f131f22f0700d53f520
Aman Sharma <mannu.poski10@gmail.com>
2020-05-28 19:44:10 +0530

    feat(*): initial setup of the project
    Signed-off-by: Aman Sharma <mannu.poski10@gmail.com>

---
+++ src/taskpane/components/App.js
@@ -0,0 +1,35 @@
+import * as React from "react";
+
+export default class App extends React.Component {
+  click = async () => {
+    return Word.run(async context => {
+      /**
+       * Insert your Word code here
+       */
+
+      // insert a paragraph at the end of the document.
+      const paragraph = context.document.body.insertParagraph("Hello World", Word.InsertLocation.end);
+
+      // change the paragraph color to blue.
+      paragraph.font.color = "green";
+
+      await context.sync();
+    });
+  };
+
+  render() {
+    const { title, isOfficeInitialized } = this.props;
+
+    if (!isOfficeInitialized) {
+      return (
+        <p>{title}: Please sideload me!</p>
+      );
+    }
+
+    return (
+      <div>
+          <button onClick={this.click}>Try clicking me!</button>
+      </div>
+    );
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,91 @@
+const devCerts = require("office-addin-dev-certs");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require('webpack');
+
+module.exports = async (env, options)  => {
+  const dev = options.mode === "development";
+  const config = {
+    devtool: "source-map",
+    entry: {
+    vendor: [
+        'react',
+        'react-dom',
+    ],
+    polyfill: 'babel-polyfill',
+    taskpane: [
+      'react-hot-loader/patch',
+      './src/taskpane/index.js',
+    ],
+    commands: './src/commands/commands.js'
+    },
+    resolve: {
+      extensions: [".html", ".js"]
+    },
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          use: [
+              'react-hot-loader/webpack',
+              'babel-loader',
+          ],
+          exclude: /node_modules/
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader']
+        },
+        {
+          test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
+          use: {
+              loader: 'file-loader',
+              query: {
+                  name: 'assets/[name].[ext]'
+                }
+              }  
+            }   
+          ]
+    },    
+    plugins: [
+      new CleanWebpackPlugin(),
+      new CopyWebpackPlugin([
+        {
+          to: "taskpane.css",
+          from: "./src/taskpane/taskpane.css"
+        }
+      ]),
+      new ExtractTextPlugin('[name].[hash].css'),
+      new HtmlWebpackPlugin({
+        filename: "taskpane.html",
+          template: './src/taskpane/taskpane.html',
+          chunks: ['taskpane', 'vendor', 'polyfill']
+      }),
+      new HtmlWebpackPlugin({
+          filename: "commands.html",
+          template: "./src/commands/commands.html",
+          chunks: ["commands"]
+      }),
+      new CopyWebpackPlugin([
+          {
+              from: './assets',
+              to: 'assets',
+          }
+      ]),
+      new webpack.ProvidePlugin({
+        Promise: ["es6-promise", "Promise"]
+      })
+    ],
+    devServer: {
+      headers: {
+        "Access-Control-Allow-Origin": "*"
+      },      
+      https: (options.https !== undefined) ? options.https : await devCerts.getHttpsServerOptions(),
+      port: process.env.npm_package_config_dev_server_port || 3000
+    }
+  };
+
+  return config;
+};
