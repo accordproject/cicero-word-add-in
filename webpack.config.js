7646a8f48878230507a0abbadea166f9becdac38
Aman Sharma <mannu.poski10@gmail.com>
2020-06-01 15:55:26 +0530

    style(webpack.config.js): add trailing commas
    Signed-off-by: Aman Sharma <mannu.poski10@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,20 @@
-const devCerts = require("office-addin-dev-certs");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
+const devCerts = require('office-addin-dev-certs');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
 
 module.exports = async (env, options)  => {
-  const dev = options.mode === "development";
+  const dev = options.mode === 'development';
   const config = {
-    devtool: "source-map",
+    devtool: 'source-map',
     entry: {
       polyfill: 'babel-polyfill',
-      main: './src/index.js'
+      main: './src/index.js',
     },
     resolve: {
-      extensions: [".html", ".js"]
+      extensions: ['.html', '.js'],
     },
     module: {
       rules: [
@@ -24,48 +24,48 @@ module.exports = async (env, options)  => {
               'react-hot-loader/webpack',
               'babel-loader',
           ],
-          exclude: /node_modules/
+          exclude: /node_modules/,
         },
         {
           test: /\.css$/,
-          use: ['style-loader', 'css-loader']
+          use: ['style-loader', 'css-loader'],
         },
         {
           test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
           use: {
               loader: 'file-loader',
               query: {
-                  name: 'assets/[name].[ext]'
-                }
-              }  
-            }   
-          ]
+                  name: 'assets/[name].[ext]',
+                },
+              },  
+            },   
+          ],
     },    
     plugins: [
       new CleanWebpackPlugin(),
       new CopyWebpackPlugin({
         patterns: [
-          { to: "index.css", from: "./src/index.css" }
-        ]
+          { to: 'index.css', from: './src/index.css' },
+        ],
       }),
       new ExtractTextPlugin('[name].[hash].css'),
       new HtmlWebpackPlugin({
-        filename: "index.html",
+        filename: 'index.html',
         template: './src/index.html',
       }),
       new CopyWebpackPlugin({
         patterns: [
-          { to: "./assets", from: "assets" }
-        ]
+          { to: './assets', from: 'assets' },
+        ],
       }),
     ],
     devServer: {
       headers: {
-        "Access-Control-Allow-Origin": "*"
+        'Access-Control-Allow-Origin': '*',
       },      
       https: (options.https !== undefined) ? options.https : await devCerts.getHttpsServerOptions(),
-      port: process.env.npm_package_config_dev_server_port || 3000
-    }
+      port: process.env.npm_package_config_dev_server_port || 3000,
+    },
   };
 
   return config;
