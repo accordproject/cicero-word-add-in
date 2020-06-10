a8407294ee34cbe8d5917377d6b50fce6bbe4579
Aman Sharma <mannu.poski10@gmail.com>
2020-06-10 20:12:49 +0530

    feat(TemplateLibrary): add function for secondary button click
    Signed-off-by: Aman Sharma <mannu.poski10@gmail.com>

--- src/components/TemplateLibrary/index.js
+++ src/components/TemplateLibrary/index.js
@@ -19,6 +19,12 @@ const LibraryComponent = () => {
     load();
   }, []);
 
+  const goToTemplateDetail = template => {
+    const templateOrigin = new URL(template.url).origin;
+    const { name, version } = template;
+    window.open(`${templateOrigin}/${name}@${version}.html`, '_blank');
+  };
+
   if(!templates){
     return <Loader active>Loading</Loader>;
   }
@@ -28,7 +34,7 @@ const LibraryComponent = () => {
       items = {templates}
       // TODO
       onPrimaryButtonClick={() => console.log('Action to add this template to contract')}
-      onSecondaryButtonClick={() => console.log('Action to view the details')}
+      onSecondaryButtonClick={template => goToTemplateDetail(template)}
     />
   );
 };
