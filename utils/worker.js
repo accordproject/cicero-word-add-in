cb89285a76e6b0ab6e1cf71af9f70fed520d3233
Aman Sharma <mannu.poski10@gmail.com>
2020-06-19 00:06:32 +0530

    feat(TemplateLibrary): add clause of template to the document
    Signed-off-by: Aman Sharma <mannu.poski10@gmail.com>

--- utils/worker.js
+++
@@ -1,8 +0,0 @@
-import { TemplateLoader } from '@accordproject/cicero-core';
-
-// Receive URL of the template and download it.
-onmessage = event => {
-  const { url } = event.data;
-  const template = TemplateLoader.fromUrl(url);
-  self.postMessage(template);
-};
