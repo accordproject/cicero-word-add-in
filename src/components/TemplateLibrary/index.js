65d00d5fd65dc00d178e0de3b69f351bb93c372f
Aman Sharma <mannu.poski10@gmail.com>
2020-06-22 20:59:06 +0530

    feat(TemplateLibrary): iterate over sample data to mark all variables
    Signed-off-by: Aman Sharma <mannu.poski10@gmail.com>

--- src/components/TemplateLibrary/index.js
+++ src/components/TemplateLibrary/index.js
@@ -26,31 +26,73 @@ const LibraryComponent = () => {
       // Inserts sample text at start
       // Ref: https://docs.microsoft.com/en-us/javascript/api/word/word.body?view=word-js-preview#inserttext-text--insertlocation-
       const contractTextRange = body.insertText(text, Word.InsertLocation.start);
-      // Search for variables from sample text
-      // Ref: https://docs.microsoft.com/en-us/javascript/api/word/word.range?view=word-js-preview#search-searchtext--searchoptions-
-      const searchResults = contractTextRange.search('"Party A"');
-      // One just needs to do it.
-      // Ref: https://youtu.be/22P43aerrho?t=511
-      searchResults.load('items/length');
-      await context.sync();
-
-      // Inserts content controls
-      for(let res=0; res<searchResults.items.length; ++res) {
-        // Insert content control where ever "Party A" is found
-        // Ref: https://docs.microsoft.com/en-us/javascript/api/word/word.range?view=word-js-preview#insertcontentcontrol--
-        let contentControl = searchResults.items[res].insertContentControl();
-        contentControl.tag = 'ship';
-        contentControl.title = 'Shipper';
-        const ooxml = contentControl.getOoxml();
-        await context.sync().then(() => {
-          if(res==0) {
-            console.log(ooxml.value);
-          }
-        });
+      // Don't search for these attribiutes in the document
+      const attributesToSkip = ['clauseId', '$class'];
+      for(const key in data) {
+        if (attributesToSkip.includes(key)) {
+          continue;
+        }
+        // Search for variables from sample text
+        // Ref: https://docs.microsoft.com/en-us/javascript/api/word/word.range?view=word-js-preview#search-searchtext--searchoptions-
+        let searchResults = contractTextRange.search(`"${data[key]}"`);
+        // One just needs to do it.
+        // Ref: https://youtu.be/22P43aerrho?t=511
+        searchResults.load('items/length');
+        await context.sync();
+        // Inserts content controls
+        for(let res=0; res<searchResults.items.length; ++res) {
+          // Insert content control where ever "Party A" is found
+          // Ref: https://docs.microsoft.com/en-us/javascript/api/word/word.range?view=word-js-preview#insertcontentcontrol--
+          let contentControl = searchResults.items[res].insertContentControl();
+          contentControl.tag = key.toLocaleLowerCase();
+          contentControl.title = `${key[0].toUpperCase()}${key.substring(1)}${res+1}`;
+        }
+        await context.sync();
+        // Add an event listener which shall respond when user changes the value of one of the variables.
+        for(let res=0; res<searchResults.items.length; ++res) {
+          Office.context.document.bindings.addFromNamedItemAsync(`${key[0].toUpperCase()}${key.substring(1)}${res+1}`, 'text', { id: `${key[0].toUpperCase()}${key.substring(1)}${res+1}` }, (res) => {
+            res.value.addHandlerAsync(Office.EventType.BindingDataChanged, handler);
+          });
+        }
+        await context.sync();
       }
-      await context.sync();
     });
   };
+
+  const handler = (event) => {
+    const { binding } = event;
+    // ID of the binding the user changed
+    const bindingId = binding.id;
+    binding.getDataAsync((result) => {
+      // The text typed by user to change it
+      const data = result.value;
+      Word.run(async context => {
+        // Get all the simlar variables
+        const contentControl = context.document.contentControls.getByTitle(bindingId).getFirst();
+        contentControl.load('tag');
+        await context.sync();
+        const tag = contentControl.tag;
+        const contentControls = context.document.contentControls.getByTag(tag);
+        contentControls.load('items/length');
+        await context.sync();
+        // To prevent it from an infinite loop, we check if text inside all the variables is same or not.
+        let contentControlText = [];
+        for(let index=0; index<contentControls.items.length; ++index) {
+          let textRange = contentControls.items[index].getRange();
+          textRange.load('text');
+          await context.sync();
+          contentControlText = [textRange.text, ...contentControlText];
+        }
+        if (contentControlText.every((el) => el === contentControlText[0])) {
+          return;
+        }
+        for(let index=0; index<contentControls.items.length; ++index) {
+          contentControls.items[index].insertText(data, Word.InsertLocation.replace);
+        }
+        return context.sync();
+      })
+    })
+  }
 
   const loadTemplateText = async url => {
     // URL to compiled archive
