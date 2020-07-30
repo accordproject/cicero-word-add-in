95f830b11d40a6c3b7c86b52b2347a291714de2c
Aman Sharma <mannu.poski10@gmail.com>
2020-07-30 15:17:19 +0530

    fix(*): render lists via inserting OOXML
    Signed-off-by: Aman Sharma <mannu.poski10@gmail.com>

--- src/utils/CiceroMarkToOOXML/index.js
+++ src/utils/CiceroMarkToOOXML/index.js
@@ -1,4 +1,6 @@
 import attachVariableChangeListener from '../AttachVariableChangeListener';
+import sanitizeHtmlChars from '../SanitizeHtmlChars';
+import unorderedListSpec from '../../constants/UnorderedListSpec';
 
 const insertHeading = async (context, value, level) => {
   const definedLevels = {
@@ -57,22 +59,71 @@ const insertVariable = async (context, title, tag, value) => {
 };
 
 const insertList = async (context, node, type) => {
-  let html = `<br><${type}>`;
+  let ooxml = '';
   node.nodes.forEach(subNode => {
-    html += `<li>${getListItem(subNode)}</li>`;
+    ooxml += `
+      <w:p>
+        <w:pPr>
+          <w:pStyle w:val="ListParagraph"/>
+          <w:numPr>
+            <w:ilvl w:val="1"/>
+            <w:numId w:val="2"/>
+          </w:numPr>
+        </w:pPr>
+        ${getListItem(subNode)}
+      </w:p>
+    `;
   });
-  // Workaround to prevent last item to fall off the list
-  html += `</${type}><br>`;
-  context.document.body.insertHtml(html, Word.InsertLocation.end);
+  ooxml = `
+    <pkg:package xmlns:pkg="http://schemas.microsoft.com/office/2006/xmlPackage">
+    <pkg:part pkg:name="/_rels/.rels" pkg:contentType="application/vnd.openxmlformats-package.relationships+xml">
+      <pkg:xmlData>
+        <Relationships xmlns="http://schemas.openxmlformats.org/package/2006/relationships">
+          <Relationship Id="rId1" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/officeDocument" Target="word/document.xml"/>
+        </Relationships>
+      </pkg:xmlData>
+    </pkg:part>
+    <pkg:part pkg:name="/word/document.xml" pkg:contentType="application/vnd.openxmlformats-officedocument.wordprocessingml.document.main+xml">
+      <pkg:xmlData>
+        <w:document xmlns:w="http://schemas.openxmlformats.org/wordprocessingml/2006/main" >
+        <w:p />
+        ${ooxml}
+        <w:p />
+        </w:document>
+      </pkg:xmlData>
+    </pkg:part>
+    ${type === 'unordered' ? unorderedListSpec : ''}
+    </pkg:package>
+  `;
+  context.document.body.insertOoxml(ooxml, Word.InsertLocation.end);
   await context.sync();
 };
 
 const getListItem = (node, text='') => {
   if (node.$class === definedNodes.text) {
-    return node.text;
+    return `
+      <w:r>
+        <w:t xml:space="preserve">${sanitizeHtmlChars(node.text)}</w:t>
+      </w:r>
+    `;
   }
   if (node.$class === definedNodes.variable) {
-    return node.value;
+    return `
+      <w:sdt>
+        <w:sdtPr>
+          <w:alias w:val="${node.name.toUpperCase()[0]}${node.name.substring(1)}"/>
+          <w:tag w:val="${node.name}"/>
+        </w:sdtPr>
+        <w:sdtContent>
+          <w:r>
+            <w:rPr>
+              <w:highlight w:val="green"/>
+            </w:rPr>
+            <w:t>${sanitizeHtmlChars(node.value)}</w:t>
+          </w:r>
+        </w:sdtContent>
+     </w:sdt>
+    `;
   }
   if (node.$class === definedNodes.softbreak) {
     return ' ';
@@ -139,10 +190,10 @@ const renderNodes = (context, node, counter, parent=null) => {
   if (node.$class === definedNodes.listBlock || node.$class === definedNodes.list) {
     switch (node.type) {
     case 'ordered':
-      insertList(context, node, 'ol');
+      insertList(context, node, 'ordered');
       return;
     case 'bullet':
-      insertList(context, node, 'ul');
+      insertList(context, node, 'unordered');
       return;
     default:
       return;
