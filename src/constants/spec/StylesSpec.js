b434c8883334fdaefeda6e9ba76b8ad39c726352
Aman Sharma <mannu.poski10@gmail.com>
2020-08-09 19:37:24 +0530

    feat(*): generate OOXML of the entire clause before rendering
    Signed-off-by: Aman Sharma <mannu.poski10@gmail.com>

---
+++ src/constants/spec/StylesSpec.js
@@ -0,0 +1,56 @@
+const styleSpec = `
+  <pkg:part pkg:name="/word/styles.xml" pkg:contentType="application/vnd.openxmlformats-officedocument.wordprocessingml.styles+xml">
+  <pkg:xmlData>
+    <w:styles xmlns:mc="http://schemas.openxmlformats.org/markup-compatibility/2006" xmlns:r="http://schemas.openxmlformats.org/officeDocument/2006/relationships" xmlns:w="http://schemas.openxmlformats.org/wordprocessingml/2006/main" xmlns:w14="http://schemas.microsoft.com/office/word/2010/wordml" xmlns:w15="http://schemas.microsoft.com/office/word/2012/wordml" xmlns:w16cid="http://schemas.microsoft.com/office/word/2016/wordml/cid" xmlns:w16se="http://schemas.microsoft.com/office/word/2015/wordml/symex" mc:Ignorable="w14 w15 w16se w16cid">
+      <w:docDefaults>
+        <w:rPrDefault>
+          <w:rPr>
+            <w:rFonts w:asciiTheme="minorHAnsi" w:eastAsiaTheme="minorHAnsi" w:hAnsiTheme="minorHAnsi" w:cstheme="minorBidi"/>
+            <w:sz w:val="22"/>
+            <w:szCs w:val="22"/>
+            <w:lang w:val="en-US" w:eastAsia="en-US" w:bidi="ar-SA"/>
+          </w:rPr>
+        </w:rPrDefault>
+        <w:pPrDefault>
+          <w:pPr>
+            <w:spacing w:after="160" w:line="259" w:lineRule="auto"/>
+          </w:pPr>
+        </w:pPrDefault>
+      </w:docDefaults>
+      <w:latentStyles w:defLockedState="0" w:defUIPriority="99" w:defSemiHidden="0" w:defUnhideWhenUsed="0" w:defQFormat="0" w:count="377">
+        <w:lsdException w:name="Normal" w:uiPriority="0" w:qFormat="1"/>
+        <w:lsdException w:name="heading 1" w:uiPriority="9" w:qFormat="1"/>
+        <w:lsdException w:name="heading 2" w:semiHidden="1" w:uiPriority="9" w:unhideWhenUsed="1" w:qFormat="1"/>
+        <w:lsdException w:name="heading 3" w:semiHidden="1" w:uiPriority="9" w:unhideWhenUsed="1" w:qFormat="1"/>
+        <w:lsdException w:name="heading 4" w:semiHidden="1" w:uiPriority="9" w:unhideWhenUsed="1" w:qFormat="1"/>
+        <w:lsdException w:name="heading 5" w:semiHidden="1" w:uiPriority="9" w:unhideWhenUsed="1" w:qFormat="1"/>
+        <w:lsdException w:name="heading 6" w:semiHidden="1" w:uiPriority="9" w:unhideWhenUsed="1" w:qFormat="1"/>
+      </w:latentStyles>
+      <w:style w:type="paragraph" w:styleId="Heading2">
+        <w:name w:val="heading 2"/>
+        <w:basedOn w:val="Normal"/>
+        <w:next w:val="Normal"/>
+        <w:link w:val="Heading2Char"/>
+        <w:uiPriority w:val="9"/>
+        <w:unhideWhenUsed/>
+        <w:qFormat/>
+        <w:rsid w:val="007A5DE1"/>
+        <w:pPr>
+          <w:keepNext/>
+          <w:keepLines/>
+          <w:spacing w:before="40" w:after="0"/>
+          <w:outlineLvl w:val="1"/>
+        </w:pPr>
+        <w:rPr>
+          <w:rFonts w:asciiTheme="majorHAnsi" w:eastAsiaTheme="majorEastAsia" w:hAnsiTheme="majorHAnsi" w:cstheme="majorBidi"/>
+          <w:color w:val="2E74B5" w:themeColor="accent1" w:themeShade="BF"/>
+          <w:sz w:val="26"/>
+          <w:szCs w:val="26"/>
+        </w:rPr>
+      </w:style>
+    </w:styles>
+  </pkg:xmlData>
+  </pkg:part>
+`;
+
+export default styleSpec;
