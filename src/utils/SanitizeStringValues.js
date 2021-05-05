8632f78f85cc87f8b42ab18fbe8e513da4cf5342
K-Kumar-01 <59891164+K-Kumar-01@users.noreply.github.com>
2021-05-05 14:34:23 +0530

    feature: Quotation Marks in Variable Values - #39
    Strip off the quotation marks in variable values
    
    Signed-off-by: K-Kumar-01 <59891164+K-Kumar-01@users.noreply.github.com>

---
+++ src/utils/SanitizeStringValues.js
@@ -0,0 +1,17 @@
+/**
+ * Removes the unnecessary quotation marks in the string
+ *
+ * @param {*} value Value to sanitize.
+ * @returns {string} Sanitized Value after replacing quotation marks
+ */
+const sanitizeStringValues = value => {
+  if (typeof value === 'string') {
+    const len = value.length;
+    if (value[0]=='"' && value[len-1]=='"') {
+      return value.substr(1, len-2);
+    }
+  }
+  return value;
+};
+
+export default sanitizeStringValues;
